6d18cd42a2831e661e016ae214729f86f687e865
deepak <deepakkumarsahoo2002@gmail.com>
2023-08-25 21:16:59 +0530

    Synced with main branch

--- flareUp/investMe/company/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Company,Category
-class CompanyAdmin():
-    list_display=('business_stage','companyName','investment_amount','equity','valuation')
-
-admin.site.register(Company)
-admin.site.register(Category)
